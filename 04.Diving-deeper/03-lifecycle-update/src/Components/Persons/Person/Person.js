84861b4e97cf3faf9a8f0150edc70f8d25999a30
Alexis <alexiscaposch15@gmail.com>
2019-05-07 23:46:14 -0300

    Added lifecycle hooks for component update

--- 04.Diving-deeper/03-lifecycle-update/src/Components/Persons/Person/Person.js
+++ 04.Diving-deeper/03-lifecycle-update/src/Components/Persons/Person/Person.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './Person.module.css';
 
-//Functional Components <---We should use this more often. It's the best practice
-const person = (props) => {
-    console.log('[Person.js] rendering...');
-    return (
-        <div className={styles.Person}>
-            <p onClick={props.click}>Hi! I am {props.name} and I'm {props.age} years old</p>
-            <input type="text" onChange={props.changed} value={props.name}></input>
-        </div>
-    );
+
+//In order to use lifecycle hooks we must change to class-based component
+class Person extends Component {
+    
+    render(){
+        console.log('[Person.js] rendering...');
+        return (
+            <div className={styles.Person}>
+                <p onClick={this.props.click}>Hi! I am {this.props.name} and I'm {this.props.age} years old</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name}></input>
+            </div>
+        );
+    }
+
 }
 
-export default person;
+export default Person;
--- 04.Diving-deeper/03-lifecycle-update/src/Components/Persons/Persons.js
+++ 04.Diving-deeper/03-lifecycle-update/src/Components/Persons/Persons.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import Person from './Person/Person'
+import Person from './Person/Person';
 
-const persons = (props) => props.persons.map((person, index) => {
-    return <Person 
-      click={() => props.clicked(index)}
-      name={person.name} 
-      age={person.age} 
-      key={person.id}
-      changed = {(event) => props.changed(event, person.id)}/>
-});
+//We must use class-based components to use lifecycle
 
-export default persons;
+class Persons extends Component {
+
+  // static getDerivedStateFromProps(props, state){
+  //   console.log('[Persons.js] getDerivedStateFromProps');
+  //   return state;
+  // }
+
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('[Persons.js] shouldComponentUpdate');
+    return true
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState){
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+    return { message: 'Snapshot!!'};
+  } 
+
+  componentDidUpdate(prevProps, prevState, snapshot){
+    console.log('[Persons.js] componentDidUpdate');
+    console.log(snapshot);
+  }
+
+
+  render(){
+    return this.props.persons.map((person, index) => {
+      console.log('rendering...');
+        return <Person 
+          click={() => this.props.clicked(index)}
+          name={person.name} 
+          age={person.age} 
+          key={person.id}
+          changed = {(event) => this.props.changed(event, person.id)}/>
+    });
+  }
+} 
+
+export default Persons;
