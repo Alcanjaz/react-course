b7d9617c3efc71d154f499b9e39c0c2db7d585df
Alexis Schmoker <alcanjaz@localhost.localdomain>
2019-05-26 22:17:10 -0300

    Added new lecture

---
+++ 04.Diving-deeper/06-optimize-with-shouldComponentUpdate/src/Components/Persons/Persons.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+import Person from './Person/Person';
+
+//We must use class-based components to use lifecycle
+
+class Persons extends Component {
+
+  // static getDerivedStateFromProps(props, state){
+  //   console.log('[Persons.js] getDerivedStateFromProps');
+  //   return state;
+  // }
+
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('[Persons.js] shouldComponentUpdate');
+    if(nextProps.persons !== this.props.persons){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState){
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+    return { message: 'Snapshot!!'};
+  } 
+
+  componentDidUpdate(prevProps, prevState, snapshot){
+    console.log('[Persons.js] componentDidUpdate');
+    console.log(snapshot);
+  }
+
+  /*For operations like cleanup that we need to execute before destroy or unmount
+  a component, we have componentWillUnmount in class-based components*/
+  componentWillUnmount(){
+    console.log('[App.js] componentWillUnmount');
+  }
+
+
+  render(){
+    return this.props.persons.map((person, index) => {
+      console.log('rendering...');
+        return <Person 
+          click={() => this.props.clicked(index)}
+          name={person.name} 
+          age={person.age} 
+          key={person.id}
+          changed = {(event) => this.props.changed(event, person.id)}/>
+    });
+  }
+} 
+
+export default Persons;
