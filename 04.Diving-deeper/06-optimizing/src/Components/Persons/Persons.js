d755a9d2eaaecc7e664146f3d08f946d5ac77364
Alexis Schmoker <alcanjaz@localhost.localdomain>
2019-05-29 00:42:40 -0300

    optimizing stuff

--- 04.Diving-deeper/06-optimizing/src/Components/Persons/Persons.js
+++ 04.Diving-deeper/06-optimizing/src/Components/Persons/Persons.js
@@ -1,24 +1,29 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
 import Person from './Person/Person';
 
 //We must use class-based components to use lifecycle
 
-class Persons extends Component {
+
+//If we want to check multiple props of our component in shoulComponentUpdate
+//React gives us a very useful tool to optimize this proccess: PureComponent
+//This component uses shouldComponentUpdate but checking multiple props without 
+// writing a control sentence.
+class Persons extends PureComponent {
 
   // static getDerivedStateFromProps(props, state){
   //   console.log('[Persons.js] getDerivedStateFromProps');
   //   return state;
   // }
 
-  shouldComponentUpdate(nextProps, nextState){
-    console.log('[Persons.js] shouldComponentUpdate');
-    if(nextProps.persons !== this.props.persons){
-      return true;
-    } else {
-      return false;
-    }
-  }
+  // shouldComponentUpdate(nextProps, nextState){
+  //   console.log('[Persons.js] shouldComponentUpdate');
+  //   if(nextProps.persons !== this.props.persons){
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
 
   getSnapshotBeforeUpdate(prevProps, prevState){
     console.log('[Persons.js] getSnapshotBeforeUpdate');
