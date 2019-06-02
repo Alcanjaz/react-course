94b1b9b6a887046cce8224d16e1fc5155b329395
Alexis Schmoker <alcanjaz@localhost.localdomain>
2019-06-02 17:42:29 -0300

    Added useRef

--- 04.Diving-deeper/10-using-refs/src/Components/Cockpit/Cockpit.js
+++ 04.Diving-deeper/10-using-refs/src/Components/Cockpit/Cockpit.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./Cockpit.module.css";
 
 //The functional components can also manage the state with the new versions of react
@@ -8,11 +8,16 @@ import styles from "./Cockpit.module.css";
 //If we want to perform side effects with functional components, now in React 16.8 we can!!
 //React Hooks allow us to do that with the useEffect Hook.
 const Cockpit = props => {
+  const toggleBtnRef = useRef(null);
+
   useEffect(() => {
     console.log("[Cockpit.js] useEffect");
-    setTimeout(() => {
-      // alert('Saved data!');
-    }, 1000);
+    // setTimeout(() => {
+    //   // alert('Saved data!');
+    // }, 1000);
+
+    toggleBtnRef.current.click();
+
     //in functional components, we can return an anonymous function
     /*this function will execute before the main useEffect function runs, 
         but after the first render cycle*/
@@ -20,6 +25,13 @@ const Cockpit = props => {
       console.log("[Cockpit.js] cleanup work in useEffect");
     };
   }, []);
+
+  useEffect(() => {
+    console.log("[Cockpit.js] 2nd useEffect");
+    return () => {
+      console.log("[Cockpit.js] cleanup work in 2nd useEffect");
+    };
+  });
 
   const classes = [];
 
@@ -40,7 +52,7 @@ const Cockpit = props => {
     <div className={styles.Cockpit}>
       <h1>{props.title}</h1>
       <p className={classes.join(" ")}>This is working :O</p>
-      <button className={btnClass} onClick={props.clicked}>
+      <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
         Toggle Persons
       </button>
     </div>
