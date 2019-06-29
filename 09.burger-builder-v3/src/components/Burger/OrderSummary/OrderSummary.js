b21ac7523666b5cd9cbaf36a0d1baf76fccae138
Alcanjaz <schmokeralexis@gmail.com>
2019-06-29 18:24:24 -0300

    updated path

---
+++ 09.burger-builder-v3/src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import Aux from "../../../hoc/Aux/Aux";
+import Button from "../../../components/UI/Button/Button";
+
+const orderSumary = props => {
+  const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
+    return (
+      <li key={igKey}>
+        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
+        {props.ingredients[igKey]}
+      </li>
+    );
+  });
+
+  return (
+    <Aux>
+      <h3>Your order:</h3>
+      <p>A delicious burger the following ingredients: </p>
+      <ul>{ingredientSummary}</ul>
+      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+      <p>Continue to checkout?</p>
+      <Button clicked={props.purchaseCanceled} buttonType="Danger">
+        CANCEL
+      </Button>
+      <Button clicked={props.purchaseContinued} buttonType="Success">
+        CONTINUE
+      </Button>
+    </Aux>
+  );
+};
+
+export default orderSumary;
