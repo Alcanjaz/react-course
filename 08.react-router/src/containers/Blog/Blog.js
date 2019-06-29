bd2a10b3cae94730d13dcf7bf8c1b2147f1c89f2
Alcanjaz <schmokeralexis@gmail.com>
2019-06-29 18:20:53 -0300

    finshed module

--- 08.react-router/src/containers/Blog/Blog.js
+++ 08.react-router/src/containers/Blog/Blog.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
-import { Route, NavLink, Switch, Redirect } from "react-router-dom";
-
-import Posts from "./Posts/Posts";
-import NewPost from "./NewPost/NewPost";
+import { Route, NavLink, Switch } from "react-router-dom";
 
 import "./Blog.css";
+import asyncComponent from "../../hoc/asyncComponent";
+import Posts from "./Posts/Posts";
+
+// import NewPost from "./NewPost/NewPost";
+const NewPost = asyncComponent(() => {
+  return import("./NewPost/NewPost");
+});
+
 
 class Blog extends Component {
   render() {
@@ -35,7 +40,8 @@ class Blog extends Component {
         <Switch>
           <Route path="/new-post" component={NewPost} />
           <Route path="/posts" component={Posts} />
-          <Redirect from="/" to="/posts" />
+          <Route render={() => <h1>Not found</h1>} />
+          {/* <Redirect from="/" to="/posts" /> */}
         </Switch>
       </div>
     );
